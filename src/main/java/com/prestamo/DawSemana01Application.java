aea9a959321ad7634c841a8262f480eee45592c6
VargasRuth0044 <i202214152@cibertec.edu.pe>
2024-09-12 18:17:01 -0500

    primer commit

---
+++ src/main/java/com/prestamo/DawSemana01Application.java
@@ -0,0 +1,13 @@
+package com.prestamo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DawSemana01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DawSemana01Application.class, args);
+	}
+
+}
